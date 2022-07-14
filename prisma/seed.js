e4341718143d661a266a250255ef339a2e17b029
Benjamin Froehlich <bdfroehlich0@gmail.com>
2022-07-13 19:49:11 -0500

    experimenting with seeder data, initial seed working

--- prisma/seed.js
+++ prisma/seed.js
@@ -3,7 +3,40 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-    console.log("seeding");
+    // await prisma.equipment.create({
+    //     data: {
+    //         name: "Air Compressor",
+    //         model: "P185",
+    //         manufacturer: "Doosan",
+    //         image: "https://media.sunbeltrentals.com/is/image/sunbeltrentals/catClass_0010030?fmt=webp",
+    //         category: "Air Compressors and Air Tools",
+    //         isAvailable: "true",
+    //         description: "Lorem ipsum..."
+    //     }
+    // })
+
+    await prisma.user.create({
+        data: {
+            email: "testuser@gmail.com",
+            name: "Joe Bob",
+            rentals: {
+                create: [
+                    {       name: "Air Compressor",
+                            model: "P185",
+                            manufacturer: "Doosan",
+                            image: "https://media.sunbeltrentals.com/is/image/sunbeltrentals/catClass_0010030?fmt=webp",
+                            category: "Air Compressors and Air Tools",
+                            description: "Lorem ipsum..."},
+                    {       name: "Double Drum Roller",
+                            model: "AR13HAR",
+                            manufacturer: "Multiquip",
+                            image: "https://media.sunbeltrentals.com/is/image/sunbeltrentals/catClass_0220120?fmt=webp",
+                            category: "Compaction",
+                            description: "Lorem ipsum..."},
+                ],
+            },
+        },
+    });
 }
 
 main().catch (e => {
