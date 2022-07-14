e327dd275a958687eb0ae5b26caf03615d7c68cf
Benjamin Froehlich <bdfroehlich0@gmail.com>
2022-07-13 20:06:56 -0500

    working on seed.js, npm install faker

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,42 +1,33 @@
 import { PrismaClient } from '@prisma/client';
+import faker from "faker";
 
 const prisma = new PrismaClient();
 
 async function main() {
-    // await prisma.equipment.create({
-    //     data: {
-    //         name: "Air Compressor",
-    //         model: "P185",
-    //         manufacturer: "Doosan",
-    //         image: "https://media.sunbeltrentals.com/is/image/sunbeltrentals/catClass_0010030?fmt=webp",
-    //         category: "Air Compressors and Air Tools",
-    //         isAvailable: "true",
-    //         description: "Lorem ipsum..."
-    //     }
-    // })
-
-    await prisma.user.create({
-        data: {
-            email: "testuser@gmail.com",
-            name: "Joe Bob",
-            rentals: {
-                create: [
-                    {       name: "Air Compressor",
-                            model: "P185",
-                            manufacturer: "Doosan",
-                            image: "https://media.sunbeltrentals.com/is/image/sunbeltrentals/catClass_0010030?fmt=webp",
-                            category: "Air Compressors and Air Tools",
-                            description: "Lorem ipsum..."},
-                    {       name: "Double Drum Roller",
-                            model: "AR13HAR",
-                            manufacturer: "Multiquip",
-                            image: "https://media.sunbeltrentals.com/is/image/sunbeltrentals/catClass_0220120?fmt=webp",
-                            category: "Compaction",
-                            description: "Lorem ipsum..."},
-                ],
+    for (let i=0; i<10; i++) {
+        await prisma.user.create({
+            data: {
+                email: `${faker.inter}`,
+                name: "Joe Bob",
+                rentals: {
+                    create: [
+                        {       name: "Air Compressor",
+                                model: "P185",
+                                manufacturer: "Doosan",
+                                image: "https://media.sunbeltrentals.com/is/image/sunbeltrentals/catClass_0010030?fmt=webp",
+                                category: "Air Compressors and Air Tools",
+                                description: "Lorem ipsum..."},
+                        {       name: "Double Drum Roller",
+                                model: "AR13HAR",
+                                manufacturer: "Multiquip",
+                                image: "https://media.sunbeltrentals.com/is/image/sunbeltrentals/catClass_0220120?fmt=webp",
+                                category: "Compaction",
+                                description: "Lorem ipsum..."},
+                    ],
+                },
             },
-        },
-    });
+        });
+    }
 }
 
 main().catch (e => {
