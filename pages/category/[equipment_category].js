40cc9a3ae535db53051bddf7607ea75e1ed67f8f
Benjamin Froehlich <bdfroehlich0@gmail.com>
2022-07-16 14:20:23 -0500

    got air compressors and air tools category working

--- pages/category/[equipment_category].js
+++ pages/category/[equipment_category].js
@@ -2,23 +2,25 @@ import { useRouter } from "next/router"
 import useSWR from "swr"
 import Card from "../../components/Card"
 
+const fetcher = (...args) => fetch(...args).then(res => res.json())
 
-export default function Equipment_id () {
+export default function equipmentCategory () {
     //let data = ['mower', 'weed eater', 'back hoe', 'tractor']
     
     const router = useRouter()
-    const equipment_id = router.query.equipment_id
     const equipment_category = router.query.equipment_category
-    const fetcher = (...args) => fetch(...args).then(res => res.json())
-    const { data, error } = useSWR('/api/equipment/', fetcher)
+    const { data, error } = useSWR(`/api/category/${equipment_category}`, fetcher)
     if (error) return <div>An error occured.</div>
     if (!data) return <div>Loading...</div>
     
     return (
         <div>
             <h1>Equipment Category: {equipment_category}</h1>
-            <h1>Equipment id: {equipment_id}</h1>
-            <Card equipment = {data}/>
+            {data.map((item) => {
+                    return (
+                        <Card equipment={item} />
+                    )
+                })}
         </div>
     )
 }
