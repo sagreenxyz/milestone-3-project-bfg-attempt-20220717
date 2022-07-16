50352487e8194d4f2bccce475eaafdc9ef383a9f
Benjamin Froehlich <bdfroehlich0@gmail.com>
2022-07-16 13:17:20 -0500

    cleaning up pages and api routes

---
+++ pages/api/equipment/[id].js
@@ -0,0 +1,16 @@
+import prisma from "../../../lib/prisma"
+
+export default async function handler(req, res) {
+
+    const { id } = req.query
+
+    try {
+        const equipmentFound = await prisma.equipment.findUnique({
+            where: {id: parseInt(id)}
+        })
+        res.status(200).json(equipmentFound)
+    } catch (err) {
+        res.status(500).json({error: `could not find data for equipment ID: ${equipmentId}`})
+    }
+
+}
