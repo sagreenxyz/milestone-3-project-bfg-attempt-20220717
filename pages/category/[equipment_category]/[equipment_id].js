ffab9aeefe8baa6a4fe4c6a250a92710a19428e0
Braxton Allen <braxtonallen99@yahoo.com>
2022-07-16 12:17:16 -0500

    created user, admin, sign up and log in pages

--- pages/category/[equipment_category]/[equipment_id].js
+++ pages/category/[equipment_category]/[equipment_id].js
@@ -1,21 +1,24 @@
 import { useRouter } from "next/router"
 import Card from "../../../components/Card"
+import useSWR from "swr"
 
 
 export default function Equipment_id () {
-    let data = ['mower', 'weed eater', 'back hoe', 'tractor']
+    //let data = ['mower', 'weed eater', 'back hoe', 'tractor']
     
     const router = useRouter()
     const equipment_id = router.query.equipment_id
     const equipment_category = router.query.equipment_category
-
-    let item = data[equipment_id]
+    const fetcher = (...args) => fetch(...args).then(res => res.json())
+    const { data, error } = useSWR('/api/equipment/by_id', fetcher)
+    if (error) return <div>An error occured.</div>
+    if (!data) return <div>Loading...</div>
     
     return (
         <div>
             <h1>Equipment Category: {equipment_category}</h1>
             <h1>Equipment id: {equipment_id}</h1>
-            <Card name ={item}/>
+            <Card equipment = {data}/>
         </div>
     )
 }
