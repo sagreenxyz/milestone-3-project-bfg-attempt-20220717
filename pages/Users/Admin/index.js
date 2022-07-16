f780029e65cfab5b22ae7cac6328c372fe0ef1af
Braxton Allen <braxtonallen99@yahoo.com>
2022-07-16 10:41:24 -0500

    npm run dev not working

---
+++ pages/Users/Admin/index.js
@@ -0,0 +1,9 @@
+
+
+export default function () {
+    return (
+        <div>
+            This is the admin page
+        </div>
+    )
+}
---
+++ pages/Users/index.js
@@ -0,0 +1,9 @@
+
+
+export default function Log_In () {
+    return (
+        <div>
+            This is the log in page
+        </div>
+    )
+}
